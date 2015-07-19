75149ced23853d355f7ddffaf6f1e4a090209e3f
lalawow <lalawow@gmail.com>
2015-07-19 16:04:53 +0800

    start. show the frame of index page. show the editor function.

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function newNote() {
+	document.getElementById('iframe_html').src = "editor.html"
+	console.log(document.getElementById('iframe_html').src)
+}
+
+function helloworld() {
+	console.log("hello world")
+}
