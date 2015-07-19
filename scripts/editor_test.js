247bf2205de06d8191885f74857485cda7cde45b
lalawow <lalawow@gmail.com>
2015-07-19 23:49:48 +0800

    use JSON and localStorage solve the notes storage

--- scripts/editor_test.js
+++ scripts/editor_test.js
@@ -1,22 +1,7 @@
-function save() {
-	var storage_content = JSON.parse(localStorage.editor_content)
-	console.log(storage_content)
-	var new_title = $('input[id="loadFileName"]').val()
-	var new_content = editor.getElement('editor').body.innerText
-	console.log("new title: "+new_title)
-	console.log("new content: "+new_content)
-	var file_id_number = storage_content["filenumber"] + 1
-	storage_content["filenumber"] ++
-	var new_json = {
-		id: file_id_number,
-		title: new_title,
-		content: new_content
-	} 
-	storage_content["content"].push(new_json)
-	localStorage.editor_content = JSON.stringify(storage_content)
-    console.log(localStorage.editor_content)
-	console.log("save OK!")
+function consolelog() {
+	console.log(editor.getElement('editor').body.innerHTML);
 }
+
 
 function old_saveFile() {
 	
