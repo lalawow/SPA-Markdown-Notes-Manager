8d8674b8bd3025143525ef34313e028f610badcc
lalawow <lalawow@gmail.com>
2015-07-21 00:46:20 +0800

    did some preview test

--- scripts/editor.js
+++ scripts/editor.js
@@ -3,6 +3,7 @@ function save() {
 	console.log(storage_content)
 	var new_title = $('input[id="loadFileName"]').val()
 	var new_content = editor.getElement('editor').body.innerText
+	var new_time = new Date()
 	console.log("new title: "+new_title)
 	console.log("new content: "+new_content)
 	var file_id_number = storage_content["filenumber"] + 1
@@ -11,6 +12,7 @@ function save() {
 		id: file_id_number,
 		title: new_title,
 		content: new_content
+		time: new_time
 	} 
 	storage_content["content"].push(new_json)
 	localStorage.editor_content = JSON.stringify(storage_content)
